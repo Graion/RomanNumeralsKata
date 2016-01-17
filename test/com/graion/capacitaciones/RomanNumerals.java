6f9569f3cb8f44530154cee14366484b350da103
aaccurso <accurso.alan@gmail.com>
2016-01-17 12:48:02 -0300

    Initial project configuration

---
+++ test/com/graion/capacitaciones/RomanNumerals.java
@@ -0,0 +1,14 @@
+package com.graion.capacitaciones;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RomanNumerals {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
