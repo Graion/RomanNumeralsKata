b275d4c1f389f7fad4e8d2d7113f1273783effa2
aaccurso <accurso.alan@gmail.com>
2016-01-17 15:59:15 -0300

    testRomanComplexSubstractionToArabic

--- src/com/graion/capacitaciones/RomanNumeral.java
+++ src/com/graion/capacitaciones/RomanNumeral.java
@@ -11,19 +11,29 @@ public class RomanNumeral {
 		this.numeral = numeral;
 		this.romanArabicTable = new Hashtable<String, Integer>();
 		this.romanArabicTable.put("I", 1);
+		this.romanArabicTable.put("IV", 4);
 		this.romanArabicTable.put("V", 5);
+		this.romanArabicTable.put("IX", 9);
 		this.romanArabicTable.put("X", 10);
+		this.romanArabicTable.put("XL", 40);
 		this.romanArabicTable.put("L", 50);
+		this.romanArabicTable.put("XC", 90);
 		this.romanArabicTable.put("C", 100);
+		this.romanArabicTable.put("CD", 400);
 		this.romanArabicTable.put("D", 500);
+		this.romanArabicTable.put("CM", 900);
 		this.romanArabicTable.put("M", 1000);
 	}
 
 	public int toArabic() {
 		Integer result = this.romanArabicTable.get(this.numeral);
 		if (result == null) {
-			String head = this.numeral.substring(0, 1);
-			String tail = this.numeral.substring(1, this.numeral.length());
+			int headLength = 1;
+			if (this.romanArabicTable.containsKey(this.numeral.substring(0, 2))) {
+				headLength = 2;
+			}
+			String head = this.numeral.substring(0, headLength);
+			String tail = this.numeral.substring(headLength, this.numeral.length());
 			return new RomanNumeral(head).toArabic() + new RomanNumeral(tail).toArabic();
 		} else {
 			return result;
--- test/com/graion/capacitaciones/RomanNumerals.java
+++ test/com/graion/capacitaciones/RomanNumerals.java
@@ -29,6 +29,31 @@ public class RomanNumerals {
 		assertEquals(new RomanNumeral("XXX").toArabic(), 30);
 		assertEquals(new RomanNumeral("CC").toArabic(), 200);
 		assertEquals(new RomanNumeral("MMM").toArabic(), 3000);
+		assertEquals(new RomanNumeral("MCXI").toArabic(), 1111);
+		assertEquals(new RomanNumeral("MDCLXVI").toArabic(), 1666);
+	}
+	
+	@Test
+	public void testRomanBasicSubstractionToArabic() {
+		assertEquals(new RomanNumeral("IV").toArabic(), 4);
+		assertEquals(new RomanNumeral("IX").toArabic(), 9);
+		assertEquals(new RomanNumeral("XL").toArabic(), 40);
+		assertEquals(new RomanNumeral("XC").toArabic(), 90);
+		assertEquals(new RomanNumeral("CD").toArabic(), 400);
+		assertEquals(new RomanNumeral("CM").toArabic(), 900);
+		assertEquals(new RomanNumeral("XIV").toArabic(), 14);
+		assertEquals(new RomanNumeral("LIX").toArabic(), 59);
+		assertEquals(new RomanNumeral("CXL").toArabic(), 140);
+		assertEquals(new RomanNumeral("DXC").toArabic(), 590);
+		assertEquals(new RomanNumeral("MCD").toArabic(), 1400);
+		assertEquals(new RomanNumeral("MCM").toArabic(), 1900);
+	}
+	
+	@Test
+	public void testRomanComplexSubstractionToArabic() {
+		assertEquals(new RomanNumeral("DXCIX").toArabic(), 599);
+		assertEquals(new RomanNumeral("MCDXIV").toArabic(), 1414);
+		assertEquals(new RomanNumeral("MCMLIX").toArabic(), 1959);
 	}
 
 }
