d82bb9814c882474d61a8d11665b2a6ce682eda6
aaccurso <accurso.alan@gmail.com>
2016-01-17 15:05:15 -0300

    testRomanBasicNumeralsToArabic

---
+++ src/com/graion/capacitaciones/RomanNumeral.java
@@ -0,0 +1,26 @@
+package com.graion.capacitaciones;
+
+import java.util.Hashtable;
+
+public class RomanNumeral {
+
+	private String numeral;
+	private Hashtable<String, Integer> romanArabicTable;
+
+	public RomanNumeral(String numeral) {
+		this.numeral = numeral;
+		this.romanArabicTable = new Hashtable<String, Integer>();
+		this.romanArabicTable.put("I", 1);
+		this.romanArabicTable.put("V", 5);
+		this.romanArabicTable.put("X", 10);
+		this.romanArabicTable.put("L", 50);
+		this.romanArabicTable.put("C", 100);
+		this.romanArabicTable.put("D", 500);
+		this.romanArabicTable.put("M", 1000);
+	}
+
+	public int toArabic() {
+		return this.romanArabicTable.get(this.numeral);
+	}
+	
+}
--- test/com/graion/capacitaciones/RomanNumerals.java
+++ test/com/graion/capacitaciones/RomanNumerals.java
@@ -7,8 +7,20 @@ import org.junit.Test;
 public class RomanNumerals {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testRomanOneToArabic() {
+		RomanNumeral romanOne = new RomanNumeral("I");
+		assertEquals(romanOne.toArabic(), 1);
+	}
+	
+	@Test
+	public void testRomanBasicNumeralsToArabic() {
+		assertEquals(new RomanNumeral("I").toArabic(), 1);
+		assertEquals(new RomanNumeral("V").toArabic(), 5);
+		assertEquals(new RomanNumeral("X").toArabic(), 10);
+		assertEquals(new RomanNumeral("L").toArabic(), 50);
+		assertEquals(new RomanNumeral("C").toArabic(), 100);
+		assertEquals(new RomanNumeral("D").toArabic(), 500);
+		assertEquals(new RomanNumeral("M").toArabic(), 1000);
 	}
 
 }
